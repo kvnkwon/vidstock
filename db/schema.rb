4456684fe6eb07d89b616225ee949579e5cb0da1
Kevin <kvnkwon@gmail.com>
2014-01-08 10:28:49 -0500

    jz add username to sign in form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140108151324) do
+ActiveRecord::Schema.define(version: 20140108152406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20140108151324) do
     t.string   "role",                   default: "user"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
